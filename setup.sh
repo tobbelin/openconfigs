f8f274043fd4c46a700af6ba9ad806535390a42c
Tobias <tobias.linghammar@gobytech.se>
2021-01-19 08:48:33 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/tobbelin/openconfigs/main/.vimrc
