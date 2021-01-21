1767e7d7c90be40ade86a7ac8435ec14f0482c58
Tobias <tobias@linghammar.se>
2021-01-21 08:07:43 +0100

    add apt commands

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+apt update
+apt install vim
+apt install shellcheck
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/tobbelin/openconfigs/main/.vimrc
+
+
