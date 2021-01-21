837247f0c0f190dfedb8f6608384abc8d8d0896d
Tobias <tobias@linghammar.se>
2021-01-21 08:09:56 +0100

    add sudo to apt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-apt update
-apt install vim
-apt install shellcheck
+sudo apt update
+sudo apt install vim
+sudo apt install shellcheck
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/tobbelin/openconfigs/main/.vimrc
